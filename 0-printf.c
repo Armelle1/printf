40f394246e1931a0d66a11bc19a42b4a1ca45266
Armelle <armelledinde@yahoo.fr>
2022-04-18 21:23:17 +0200

    Fix

--- 0-printf.c
+++ 0-printf.c
@@ -2,6 +2,7 @@
 #include <unistd.h>
 #include <stdio.h>
 #include "main.h"
+#include <string.h>
 
 /**
  *write_nbr - write number
@@ -29,6 +30,24 @@ int write_nbr(int n)
 	return (1);
 }
 /**
+ *write_unsign_nbr - write number
+ *@n : the number that written
+ * Return: The written character
+ */
+int write_unsign_nbr(unsigned int n)
+{
+  char c;
+
+  if (n > 9)
+    {
+      write_nbr(n / 10);
+      return (1 + write_nbr(n % 10));
+    }
+  c = n + '0';
+  write(1, &c, 1);
+  return (1);
+}
+/**
  *_printf - print args
  *@format: 1st param
  * Return: the number of characters printed
@@ -38,7 +57,7 @@ int _printf(const char *format, ...)
 	va_list p;
 	int  d, bytes_written = 0;
 	char c, *s;
-	unsigned int i;
+	unsigned int u;
 
 	if (format == NULL)
 		return (0);
@@ -60,15 +79,15 @@ int _printf(const char *format, ...)
 					break;
 				case 's':
 					s = va_arg(p, char *);
-					bytes_written += write(1, s, sizeof(s));
+					bytes_written += write(1, s, strlen(s));
 					break;
 				case 'd':
 					d = va_arg(p, int);
 					bytes_written += write_nbr(d);
 					break;
-				case 'i':
-					i = va_arg(p, unsigned int);
-					bytes_written += write_nbr(i);
+				case 'u':
+					u = va_arg(p, unsigned int);
+					bytes_written += write_unsign_nbr(u);
 					break;
 				default:
 					bytes_written += write(1, &c, 1);
--- main.h
+++ main.h
@@ -2,4 +2,5 @@
 #define MAIN_H
 int _printf(const char *format, ...);
 int write_nbr(int n);
+int write_unsign_nbr(unsigned int n);
 #endif
